746bc453f79cea7adb4094e8afd05880610fe077
ayush-a2 <ayush8583@gmail.com>
2022-12-16 11:39:06 +0530

    fw21_0574

---
+++ AdminSide/components/admin_navbar.js
@@ -0,0 +1,12 @@
+let admin_navbar=()=>{
+    return `   
+    <h3><a href="./dashboard.html" class="adminavHeading">Home</a></h3>
+    <!-- <h3><a href="" class="adminavHeading">Collection</a></h3> -->
+    <h3><a href="./indutrial.html" class="adminavHeading">Industrial</a></h3>
+    <h3><a href="./cell.html" class="adminavHeading">Cell Phones</a></h3>
+    <!-- <h3><a href="" class="adminavHeading">Collection</a></h3> -->
+    <h3><a href="./sport.html" class="adminavHeading">Sports</a></h3>
+    <h3><a href="./appliance.html" class="adminavHeading">Appliances</a></h3>
+`
+}
+export {admin_navbar};
