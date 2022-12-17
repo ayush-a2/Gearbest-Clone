90f7f642dd3f0f309716bfd0f5e5146be70310a4
Ritikve03 <ritik.rv03@gmail.com>
2022-12-17 16:17:12 +0530

    fw21_0712

---
+++ side-navbar/sidenav.js
@@ -0,0 +1,19 @@
+let a1 = document.getElementById("cell");
+let a2 = document.getElementById("laptops");
+let a3 = document.getElementById("consumer");
+let a4 = document.getElementById("computer");
+let a5 = document.getElementById("printers");
+let a6 = document.getElementById("home");
+let a7 = document.getElementById("appliances");
+let a8 = document.getElementById("sports");
+
+let s1 = document.getElementById("sub1");
+let s2 = document.getElementById("sub2");
+let s3 = document.getElementById("sub3");
+let s4 = document.getElementById("sub4");
+let s5 = document.getElementById("sub5");
+let s6 = document.getElementById("sub6");
+let s7 = document.getElementById("sub7");
+let s8 = document.getElementById("sub8");
+
+
