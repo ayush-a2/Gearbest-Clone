5a14ad5b5351cca5e502a33aad59e703daa52847
Ritikve03 <ritik.rv03@gmail.com>
2022-12-18 22:00:33 +0530

    navbar-2 completed

--- side-navbar/sidenav.js
+++ side-navbar/sidenav.js
@@ -1,17 +1,112 @@
-let a1 = document.getElementById("cell");
-let a2 = document.getElementById("laptops");
-let a3 = document.getElementById("consumer");
-let a4 = document.getElementById("computer");
-let a5 = document.getElementById("printers");
-let a6 = document.getElementById("home");
-let a7 = document.getElementById("appliances");
-let a8 = document.getElementById("sports");
+let a = document.getElementById("dropbtn");
+let b = document.getElementById("drpdwn");
+a.addEventListener("mouseenter",visi)
+b.addEventListener("mouseenter",visi)
+a.addEventListener("mouseleave",hid)
+b.addEventListener("mouseleave",hid)
 
-let s1 = document.getElementById("sub1");
-let s2 = document.getElementById("sub2");
-let s3 = document.getElementById("sub3");
-let s4 = document.getElementById("sub4");
-let s5 = document.getElementById("sub5");
-let s6 = document.getElementById("sub6");
-let s7 = document.getElementById("sub7");
-let s8 = document.getElementById("sub8");
+function visi(){
+    document.getElementById("drpdwn").style.visibility = "visible";
+
+}
+
+function hid(){
+    document.getElementById("drpdwn").style.visibility ="hidden"
+}
+
+// ----------------------------------------------
+
+let sub_inner1 = document.getElementById("accessories")
+sub_inner1.addEventListener("mouseenter",firstvisible)
+
+sub_inner1.addEventListener("mouseleave",firsthidden)
+
+function firstvisible(){
+    document.getElementById("sub-inner1").style.visibility = "visible"
+}
+
+function firsthidden(){
+    document.getElementById("sub-inner1").style.visibility = "hidden"
+}
+
+
+let sub_inner2 = document.getElementById("laptop")
+sub_inner2.addEventListener("mouseenter",secondvisible)
+
+sub_inner2.addEventListener("mouseleave",secondhidden)
+
+function secondvisible(){
+    document.getElementById("sub-inner2").style.visibility = "visible"
+}
+
+function secondhidden(){
+    document.getElementById("sub-inner2").style.visibility = "hidden"
+}
+
+
+let sub_inner3 = document.getElementById("electronics");
+sub_inner3.addEventListener("mouseenter",thirdvisible);
+sub_inner3.addEventListener("mouseleave",thirdhidden);
+
+function thirdvisible(){
+    document.getElementById("sub-inner3").style.visibility = "visible"
+}
+function thirdhidden(){
+    document.getElementById("sub-inner3").style.visibility = "hidden"
+}
+
+let sub_inner4 = document.getElementById("computer");
+sub_inner4.addEventListener("mouseenter",forthvisible);
+sub_inner4.addEventListener("mouseleave",forthhidden);
+
+function forthvisible(){
+    document.getElementById("sub-inner4").style.visibility = "visible"
+}
+function forthhidden(){
+    document.getElementById("sub-inner4").style.visibility = "hidden"
+}
+
+
+let sub_inner5 = document.getElementById("printer");
+sub_inner5.addEventListener("mouseenter",fifthvisible);
+sub_inner5.addEventListener("mouseleave",fifthhidden);
+
+function fifthvisible(){
+    document.getElementById("sub-inner5").style.visibility = "visible"
+}
+function fifthhidden(){
+    document.getElementById("sub-inner5").style.visibility = "hidden"
+}
+
+let sub_inner6 = document.getElementById("garden");
+sub_inner6.addEventListener("mouseenter",sixthvisible);
+sub_inner6.addEventListener("mouseleave",sixthhidden);
+
+function sixthvisible(){
+    document.getElementById("sub-inner6").style.visibility = "visible"
+}
+function sixthhidden(){
+    document.getElementById("sub-inner6").style.visibility = "hidden"
+}
+
+let sub_inner7 = document.getElementById("appli");
+sub_inner7.addEventListener("mouseenter",seventhvisible);
+sub_inner7.addEventListener("mouseleave",seventhhidden);
+
+function seventhvisible(){
+    document.getElementById("sub-inner7").style.visibility = "visible"
+}
+function seventhhidden(){
+    document.getElementById("sub-inner7").style.visibility = "hidden"
+}
+
+let sub_inner8 = document.getElementById("sports");
+sub_inner8.addEventListener("mouseenter",eigthvisible);
+sub_inner8.addEventListener("mouseleave",eigthhidden);
+
+function eigthvisible(){
+    document.getElementById("sub-inner8").style.visibility = "visible"
+}
+function eigthhidden(){
+    document.getElementById("sub-inner8").style.visibility = "hidden"
+}
